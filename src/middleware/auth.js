f56e690027e41482350e06e565d3051a4be63e7d
Azurus333 <mxm.prs31@gmail.com>
2026-04-02 15:49:15 +0200

    b

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,2 +1,2 @@
 // Shim: reuse backend auth middleware for src-based server wiring
-module.exports = require('../backend/src/middleware/auth');
+module.exports = require('../../backend/src/middleware/auth');
